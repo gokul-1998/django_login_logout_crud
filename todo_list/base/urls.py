3314ba9d65f82fcf42c229c0cbdb81bf51bc67d7
gokul-1998 <gokulakrishnanm1998@gmail.com>
2024-12-26 22:01:07 +0530

    34 done

--- todo_list/base/urls.py
+++ todo_list/base/urls.py
@@ -1,7 +1,8 @@
 from django.urls import path
 
-from .views import Tasklist
+from .views import Tasklist, TaskDetail
 
 urlpatterns = [
     path('', Tasklist.as_view(), name='tasks'),
+    path('task/<int:pk>', TaskDetail.as_view(), name='task'),
 ]
--- todo_list/base/views.py
+++ todo_list/base/views.py
@@ -2,7 +2,14 @@ from django.http import HttpResponse
 from django.shortcuts import render
 from .models import Task
 from django.views.generic.list import ListView
+from django.views.generic.detail import DetailView
 # Create your views here.
 
 class Tasklist(ListView):
     model = Task
+    context_object_name = 'tasks'
+
+class  TaskDetail(DetailView):
+    model = Task
+    context_object_name = 'task'
+    template_name = 'base/task.html'
