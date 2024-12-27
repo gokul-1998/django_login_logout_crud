5ebf55a9fefe0b40cff554adc91639681912a29d
gokul-1998 <gokulakrishnanm1998@gmail.com>
2024-12-27 06:23:06 +0530

    task delete bug

--- todo_list/base/urls.py
+++ todo_list/base/urls.py
@@ -1,10 +1,11 @@
 from django.urls import path
 
-from .views import Tasklist, TaskDetail, TaskCreate, TaskUpdate
+from .views import Tasklist, TaskDetail, TaskCreate, TaskUpdate,DeleteView
 
 urlpatterns = [
     path('', Tasklist.as_view(), name='tasks'),
     path('task/<int:pk>', TaskDetail.as_view(), name='task'),
     path('task-create/', TaskCreate.as_view(), name='task-create') ,
     path('task-update/<int:pk>', TaskUpdate.as_view(), name='task-update'),
+    path('task-delete/<int:pk>', DeleteView.as_view(), name='task-delete')
 ]
--- todo_list/base/views.py
+++ todo_list/base/views.py
@@ -5,6 +5,7 @@ from django.views.generic.list import ListView
 from django.views.generic.detail import DetailView
 from django.views.generic.edit import CreateView, UpdateView
 from django.urls import reverse_lazy
+from django.views.generic.edit import DeleteView
 
 # Create your views here.
 
@@ -25,4 +26,9 @@ class TaskCreate(CreateView):
 class TaskUpdate(UpdateView):
     model=Task
     fields='__all__'
+    success_url=reverse_lazy('tasks')
+
+class DeleteView(DeleteView):
+    model=Task
+    context_object_name = 'task'
     success_url=reverse_lazy('tasks')
